c9f8df8f7ec5ea980c242a3ba6b8934a08d736a9
bliestek <66104179+bliestek@users.noreply.github.com>
2025-05-05 23:09:59 +0930

    Add files via upload

--- app.js
+++ app.js
@@ -89,8 +89,7 @@ function getSelectedMapIndex(rotation) {
     const numMaps = maps.length;
     const anglePerSlice = 360 / numMaps;
     const normalizedRotation = (rotation % 360 + 360) % 360; // Normalize rotation to 0-360 degrees
-    const arrowAngle = 0; // Arrow is at the top (0 degrees)
-    const adjustedRotation = (normalizedRotation + arrowAngle) % 360; // Adjust rotation to align with the arrow
+    const adjustedRotation = (normalizedRotation + anglePerSlice / 2) % 360; // Adjust rotation to align with the top
     return Math.floor(adjustedRotation / anglePerSlice);
 }
 
